ef4da27569f591084b43da3726b47145689b6db4
Blake Barnes <ellisbarnes00@gmail.com>
2017-10-04 04:54:47 -0400

    Added a test to see if it could move forward

--- src/org/usfirst/frc/team4065/robot/Robot.java
+++ src/org/usfirst/frc/team4065/robot/Robot.java
@@ -2,12 +2,15 @@ package org.usfirst.frc.team4065.robot;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 
 import org.usfirst.frc.team4065.robot.RobotMap;
 
 public class Robot extends IterativeRobot {
 
+	RobotDrive roybot = RobotMap.robot;
+	
 	public void robotInit() {
 		// TODO: Figure out which camera is forward and backwards
 		CameraServer.getInstance().startAutomaticCapture(0);
@@ -15,10 +18,19 @@ public class Robot extends IterativeRobot {
 		
 		RobotMap.init();
 	}
+
+	public void autonomousPeriodic() {
+		while(isAutonomous() && isEnabled()) {
+			roybot.arcadeDrive(-0.5, 0.5);
+			
+			//RobotMap.motors.get(1).set(1);
+			//RobotMap.motors.get(2).set(-1);
+		}
+	}
 	
 	public void teleopPeriodic() {
 		while(isOperatorControl() && isEnabled()) {
-			RobotMap.robot.arcadeDrive(RobotMap.controller.getRawAxis(1), RobotMap.controller.getRawAxis(2));
+			roybot.arcadeDrive(RobotMap.controller.getRawAxis(1), RobotMap.controller.getRawAxis(2));
 			Timer.delay(0.01);
 		}
 	}
